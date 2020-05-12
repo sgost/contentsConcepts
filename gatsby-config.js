4becdac5799e12b0a2c43af202c7bb5e06ecc9f1
Mohanapriya <priyamecheri@gmail.com>
2020-05-12 08:04:32 +0530

    Initial Commit * updated readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Content Concepts`,
+    description: `Top-notch editing services.`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +17,30 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `content-concepts`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-antd',
+      options: {
+        style: true
+      }
+    },
+    {
+      resolve: `gatsby-plugin-less`,
+      options: {
+        javascriptEnabled: true
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/styles/variables.js
@@ -0,0 +1,42 @@
+// fonts
+export const FONT_FAMILY = 'Poppins, sans-serif'
+export const CONTENT_FONT_FAMILY = 'Poppins, sans-serif'
+
+// colors
+export const WHITE_COLOR = '#FFFFFF';
+export const BLACK_COLOR = '#000000';
+export const ACCENT_GREEN = '#98ECA6';
+export const ACCENT_PORTICA = '#F5DA7D';
+export const ACCENT_TEAL = '#98E5E0';
+export const ACCENT_RED = '#FFABAB';
+export const COLOR_DENIM = '#1565C0';
+
+// brand colors
+export const APP_BRAND_COLOR = '#5860AF';
+export const APP_BACKGROUND_COLOR = '#FFFFFF';
+export const APP_HEADER_BACKGROUND_COLOR = '#FFF';
+export const PRIMARY_SECTION_BACKGROUND = '#F5FBFF';
+export const SECONDARY_SECTION_BACKGROUND = '#16386C';
+
+// border colors
+export const BORDER_COLOR = '#CCCCCC';
+
+// text colors
+export const PRIMARY_TXT_COLOR = '#333333';
+export const SECONDARY_TXT_COLOR = '#666666';
+export const HEADER_TXT_COLOR = '#183247';
+export const NAVLINK_COLOR = '#555555';
+export const HIGHLIGHT_TEXT = '#27AE60';
+
+// button colors
+export const BUTTON_PRIMARY = '#1976D2';
+
+// icon colors
+export const ICON_COLOR = '#FFF';
+export const ICON_SELECTED_BACKGROUND_COLOR = 'rgba(255, 255, 255, 0.3)';
+export const ICON_BACKGROUND = '#BBDEFB';
+
+//status colors
+export const HIGH = '#18D7C7';
+export const MEDIUM = '#FED82B';
+export const LOW = '#FF6766';

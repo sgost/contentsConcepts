8e0ee382fcfc6739b7c2b456ab5af48858cb8cd1
Mohanapriya <priyamecheri@gmail.com>
2020-05-22 17:24:03 +0530

    added github url for fetching data

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/data`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,49 +1,87 @@
-import React from "react"
+import React, { useState, useEffect, Fragment } from "react"
+
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-
 import Banner from "../components/Banner"
-import bannerContent from "../data/banner.json"
-
 import Features from "../components/Features"
-import featuresContent from "../data/features.json"
-
 import Clients from "../components/Clients"
-import clientsContent from "../data/clients.json"
-
 import Services from "../components/Services"
-import servicesContent from "../data/services.json"
-
 import Specialization from "../components/Specialization"
-import specializationContent from "../data/specialization.json"
-
 import Testimonial from "../components/Testimonial"
-import testimonialContent from "../data/testimonial.json"
-
 import Process from "../components/Process"
-import processContent from "../data/process.json"
-
 import Customers from "../components/Customers"
-import customersContent from "../data/customers.json"
-
 import Contact from "../components/Contact"
-import contactContent from "../data/contact.json"
 
 const HomePage = () => {
+
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+  const jsonUrl = 'https://mohanapriya1994.github.io/ContentConceptsData/data/';
+
+  const[loading, setLoading] = useState(true);
+
+  const[data, setData] = useState({
+    banner: {},
+    features: [],
+    clients: {},
+    services: {},
+    specialization: {},
+    testimonial: [],
+    process: {},
+    customers: {},
+    contact: {}
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const bannerData = await fetch(jsonUrl + 'banner.json').then(res => res.json());
+      const featuresData = await fetch(jsonUrl + 'features.json').then(res => res.json());
+      const clientsData = await fetch(jsonUrl + 'clients.json').then(res => res.json());
+      const servicesData = await fetch(jsonUrl + 'services.json').then(res => res.json());
+      const specializationData = await fetch(jsonUrl + 'specialization.json').then(res => res.json());
+      const testimonialData = await fetch(jsonUrl + 'testimonial.json').then(res => res.json());
+      const processData = await fetch(jsonUrl + 'process.json').then(res => res.json());
+      const customersData = await fetch(jsonUrl + 'customers.json').then(res => res.json());
+      const contactData = await fetch(jsonUrl + 'contact.json').then(res => res.json());
+
+      setData({
+        ...data,
+        banner: bannerData,
+        features: featuresData,
+        clients: clientsData,
+        services: servicesData,
+        specialization: specializationData,
+        testimonial: testimonialData,
+        process: processData,
+        customers: customersData,
+        contact: contactData
+      });
+      setLoading(false);
+    }
+    fetchData()
+  }, [])
+
   return (
-    <Layout>
-      <SEO title="Home" />
-      <Banner content={bannerContent} />
-      <Features content={featuresContent} />
-      <Clients content={clientsContent} />
-      <Services content={servicesContent} />
-      <Specialization content={specializationContent} />
-      <Testimonial content={testimonialContent} />
-      <Process content={processContent} />
-      <Customers content={customersContent} />
-      <Contact content={contactContent} />
-    </Layout>
+    <Fragment>
+      { loading && <Spin indicator={antIcon} className="homeSpin" /> }
+      { !loading &&
+        <Layout>
+          <SEO title="Home" />
+          <Banner content={data.banner} />
+          <Features content={data.features} />
+          <Clients content={data.clients} />
+          <Services content={data.services} />
+          <Specialization content={data.specialization} />
+          <Testimonial content={data.testimonial} />
+          <Process content={data.process} />
+          <Customers content={data.customers} />
+          <Contact content={data.contact} />
+        </Layout>
+      }
+    </Fragment>
   )
 }
 

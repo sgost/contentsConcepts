e508f7eaa84895eb6cc8089a86c0776b1008f899
Manoj <manoj@fidisys.com>
2021-08-04 13:34:06 +0530

    changes done in time

--- src/components/PriceEstimate/index.js
+++ src/components/PriceEstimate/index.js
@@ -7,14 +7,51 @@ import {
 } from './styles';
 import { Form, Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import {Link} from "gatsby";
 
 const PriceEstimate = () => {
-    const time = new Date().toLocaleTimeString();
-    const dayNumber = new Date().getDate();
-    const year = new Date().getFullYear();
-    const dayName = new Date().toLocaleString("default", { weekday: "long" });
-    const monthName = new Date().toLocaleString("default", { month: "long" });
-  
+    //Prize array
+    const array =
+        [
+            {
+                id: 1,
+                title: "Proof Reading & Editing",
+                dollers: "0.020",
+                rupees: "1.50",
+            },
+            {
+                id: 2,
+                title: "Substantive Editing",
+                dollers: "0.024",
+                rupees: "1.75",
+            },
+            {
+                id: 1,
+                title: "Plagarism Editing",
+                dollers: "0.034",
+                rupees: "2.50",
+            },
+        ]
+
+    const proceedtopay = paymap => {
+
+    }
+    const [pay, setpay] = useState(null)
+    const [toggleState, setToggleState] = useState(0)
+    const [currency, setcurrency] = useState(1)
+
+    const toggleTab = index => {
+        setToggleState(index)
+    }
+
+    const UpdateTime = pay <= 2000 ? 48 : pay === 2001 || pay <= 4000 ? 96 : pay === 4001 || pay <= 6000 ? 144 : pay > 6000 ? 192 : "";
+    var d = new Date();
+    const kk = d.setHours(UpdateTime);
+    const dayNumber = d.getDate();
+    const year = d.getFullYear();
+    const dayName = d.toLocaleString("default", { weekday: "long" });
+    const monthName = d.toLocaleString("default", { month: "long" });
+
     const normFile = e => {
         if (Array.isArray(e)) {
             return e;
@@ -59,15 +96,25 @@ const PriceEstimate = () => {
                     <div id="slide_label">
                         <label htmlFor="firstName">How many words do you want to edit ?</label>
                         <div id="slider_buttons">
-                            <input type="text" id="peinput" value="450" />
+                            <input type="text" id="peinput" value={pay}
+                                onChange={e => setpay(e.target.value)} />
                         </div>
                     </div>
                     <div id="slide_label">
                         <label htmlFor="firstName">Choose your service</label>
                         <div id="slider_buttons">
-                            <button>Proof Reading & Editing</button>
-                            <button>Substantive Editing</button>
-                            <button>Plagarism Editing</button>
+                            {array && array.map((paymap, i) =>
+                                <>
+                                    <button key={i}
+                                        className={
+                                            toggleState === i
+                                                ? "pay_block2_container1 pay_block2_container2"
+                                                : "pay_block2_container1"
+                                        }
+                                        onClick={() => toggleTab(i)}
+                                    >{paymap.title}</button>
+                                </>
+                            )}
                         </div>
                     </div>
                     <div id="slide_label">
@@ -87,23 +134,35 @@ const PriceEstimate = () => {
                         </Form.Item>
                     </div>
                 </PriceEstimate_container_b1>
-                <PriceEstimate_container_b2>
-                    <div id="p_b_top_main">
-                        <button>₹ INR </button>
-                        <button>$ USD</button>
-                    </div>
-                    <div id="p_b_top">
-                        <h1>Total Price</h1>
-                        <h2>₹ 4500</h2>
-                    </div>
-                    <div id="p_b_middle">
-                        <h1>Returned before</h1>
-                        <h1>{dayName}, {monthName}{dayNumber}, {year}, {time}</h1>
-                    </div>
-                    <div id="p_b_bottom">
-                        <button>Make Payment</button>
-                    </div>
-                </PriceEstimate_container_b2>
+                {array && array.map((paymap, i) => (
+                    <>
+                        {toggleState === i && (
+                            <PriceEstimate_container_b2 key={i}>
+                                <div id="p_b_top_main">
+                                    <button onClick={() => setcurrency(1)} className={currency === 1 ? "currency1 currency2" : "currency1"}>₹ INR </button>
+                                    <button onClick={() => setcurrency(2)} className={currency === 2 ? "currency1 currency2" : "currency1"}>$ USD</button>
+                                </div>
+                                <div id="p_b_top">
+                                    <h1>Total Price</h1>
+                                    {(currency === 1) ?
+                                        <h2>₹ {paymap.rupees * pay}</h2>
+                                        :
+                                        <h2>$ {paymap.dollers * pay}</h2>
+                                    }
+                                </div>
+                                <div id="p_b_middle">
+                                    <h1>Returned before</h1>
+                                    <h1>{dayName}, {monthName} {dayNumber}, {year}</h1>
+                                </div>
+                                <div id="p_b_bottom">
+                                    <Link to = "/pricing/#pays"><button onClick={() => {
+                                        proceedtopay(paymap)
+                                    }}>Make Payment</button></Link>
+                                </div>
+                            </PriceEstimate_container_b2>
+                        )}
+                    </>
+                ))}
             </PriceEstimate_container>
         </PriceEstimate_main>
     )

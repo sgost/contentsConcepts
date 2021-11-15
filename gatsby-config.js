0b7fb723edfe5fc40a8d75b373145c69999d7372
sgost <jameslucky917@gmail.com>
2021-11-15 11:58:37 +0530

    changes done in banner

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
       options: {
         name: `contentconcepts-data`,
         remote: `https://github.com/fidisys/contentconcepts-data.git`,
-        branch: `newFeatures2`
+        branch: `master`
       }
     },
     `gatsby-remark-reading-time`,
